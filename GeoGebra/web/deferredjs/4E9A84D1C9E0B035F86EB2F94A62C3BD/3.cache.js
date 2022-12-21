5c9d853369779b60ced9dada0e040a1e0f0d29b6
Aober <aober528@163.com>
2022-12-21 22:52:26 +0800

    add GeoGebra

---
+++ GeoGebra/web/deferredjs/4E9A84D1C9E0B035F86EB2F94A62C3BD/3.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback3('rAc(3476,1,wCm);_.te=function(){};eOl(ki)(3);\n//# sourceURL=web-3.js\n')
---
+++ GeoGebra/webSimple/deferredjs/E6351DF21E505A2BD262FCB643FBA42E/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('Yzb(1950,1,pDh);_.zd=function(){};N1g(Ng)(2);\n//# sourceURL=webSimple-2.js\n')
